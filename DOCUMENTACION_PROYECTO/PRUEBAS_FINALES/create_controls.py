d2b8d0a2b3503d6f53c5ce546b3a8be56f603bd1
stakion <106846382+stakion@users.noreply.github.com>
2023-12-11 12:51:26 -0600

    Add files via upload

---
+++ DOCUMENTACION_PROYECTO/PRUEBAS_FINALES/create_controls.py
@@ -0,0 +1,160 @@
+from ISA import instruction_map
+
+keys_data = list(instruction_map.keys())
+intr_data = list(instruction_map.values())
+intr_data.sort( key=lambda a: len(a[1]))
+
+directory = "CIRCUITOS_SEPARADOS"
+
+
+# from page 397 from the book
+control_codes = "Reg2Loc ALUOp ALUSrc Branch MemRead MemWrite RegWrite MemtoReg SExt UBranch EsEstoUnMOV".split(" ")
+R_fmt =    [0,2,0,0,0,0,1,0,0,0,0]
+I_fmt =    [0,2,1,0,0,0,1,0,3,0,0]
+IW_fmt =   [0,0,0,0,0,0,1,0,0,0,1]
+LDUR_fmt = [0,0,1,0,1,0,1,1,0,0,0]
+STUR_fmt = [1,0,1,0,0,1,0,0,0,0,0]
+CBZ_fmt =  [1,1,0,1,0,0,0,0,2,0,0]
+B_fmt =    [1,1,0,1,0,0,0,0,1,1,0]
+# B_fmt =    []
+# MOVZ  =    []
+
+dict_fmt = {
+    "LDUR": LDUR_fmt,
+    "STUR": STUR_fmt,
+    "CBZ" : CBZ_fmt,
+    "B" : B_fmt
+}
+
+alu_codes = "Op1 Op0 Ainv Binv".split(" ")
+dict_ALOps = {
+    "LDUR": [1,0,0,0],
+    "STUR": [1,0,0,0],
+    "CBZ" : [1,1,0,1],
+}
+dict_ALOps_R = {
+    "AND" : [0,0,0,0],
+    "ORR" : [0,1,0,0],
+    "ADD" : [1,0,0,0],
+    "SUB" : [1,0,0,1],
+    "XOR" : [1,1,0,0],
+    "NOR" : [0,0,1,1]
+}
+
+def print_header(f, name:str, ccodes=control_codes, args="opcode"):
+    print("function [", end="", file=f)
+    for j, ctrl in enumerate(ccodes):
+        if j == len(ccodes)-1:
+            print(f"{ctrl}] = {name}({args})", file=f)
+        else:
+            print(f"{ctrl}, ", end="", file=f)
+
+def print_base(f, ccodes=control_codes):
+    for ctrl in ccodes:
+        print(f"\t{ctrl} = 0;",file=f)
+
+def print_iftrue(f, names, values, prefix="\t\t"):
+    for i,val in enumerate(values):
+        if val == 0: continue
+        print(f"{prefix}{names[i]} = {val};", file=f)
+
+def is_not_inside(outer, inner) -> bool:
+    if len(inner) == 1: return True
+    if outer == "LSL": return True
+    if outer == "LSR": return True
+    res = (inner in outer)
+    return not res
+
+def alu_control():
+    out_file = f"{directory}/alu_control.m"
+    with open(out_file, "w") as f:
+        # header
+        print_header(f, "alu_control", alu_codes, "ALUOp, opcode")
+        print_base(f, ccodes=alu_codes)
+
+        print("\topcode_R = xl_slice(opcode, 31, 21);", file=f)
+        print("\topcode_I = xl_slice(opcode, 31, 22);", file=f)
+
+        # poner los codes apropiados
+        print("\tswitch ALUOp", file=f)
+        print("\t\tcase 0 % LDUR y STUR", file=f)
+        print_iftrue(f, alu_codes, dict_ALOps["LDUR"], prefix="\t\t\t")
+        print("\t\tcase 1 % CBZ", file=f)
+        print_iftrue(f, alu_codes, dict_ALOps["CBZ"], prefix="\t\t\t")
+        print("\t\tcase 2 % Formato R e I", file=f)
+        # checar por toda la ISA
+        print("\t\t\tswitch opcode_R", file=f)
+        for name in instruction_map.keys():
+            for alu_name in dict_ALOps_R.keys(): # checar por si se parece algun op
+                if is_not_inside(name, alu_name): continue
+                if instruction_map[name][0] == "I": continue
+                print(f"\t\t\t\tcase {int(instruction_map[name][1],2)} % {name}", file=f)
+                print_iftrue(f, alu_codes, dict_ALOps_R[alu_name], prefix="\t\t\t\t\t")
+        print("\t\t\tend", file=f)
+        print("\t\t\tswitch opcode_I", file=f)
+        for name in instruction_map.keys():
+            for alu_name in dict_ALOps_R.keys(): # checar por si se parece algun op
+                if is_not_inside(name, alu_name): continue
+                if instruction_map[name][0] == "R": continue
+                print(f"\t\t\t\tcase {int(instruction_map[name][1],2)} % {name}", file=f)
+                print_iftrue(f, alu_codes, dict_ALOps_R[alu_name], prefix="\t\t\t\t\t")
+        print("\t\t\tend", file=f)
+        print("\tend", file=f)
+        print("end", file=f)
+        print(f"[INFO] We wrote the alu controls to '{out_file}' succesfully!")
+
+
+def main_control():
+    first = True
+    out_file = f"{directory}/control.m"
+    current_key = ""
+    with open(out_file, "w") as f:
+        # header
+        print_header(f, "control_block")
+        print_base(f)
+
+        for i,el in enumerate(intr_data):
+            # start switch case
+            if el[0] != current_key:
+                if not first:
+                    print("\tend\n", file=f) # end del switch
+                else:
+                    first = False
+                current_key = el[0]
+                print(f"\t% Formato {current_key}", file=f)
+                print(f"\ttemp = xl_slice(opcode, 31, {31-len(el[1])+1});", file=f)
+                print(f"\tswitch temp", file=f)
+            print(f"\t\tcase {int(el[1],2)} % {keys_data[i]}", file=f)
+
+            # flags
+            if current_key == "R":
+                for j, code in enumerate(control_codes):
+                    if R_fmt[j] == 0: continue
+                    print(f"\t\t\t{code} = {R_fmt[j]};", file=f)
+                continue
+            if current_key == "I":
+                for j, code in enumerate(control_codes):
+                    if I_fmt[j] == 0: continue
+                    print(f"\t\t\t{code} = {I_fmt[j]};", file=f)
+                continue
+            if current_key == "IW":
+                for j, code in enumerate(control_codes):
+                    if IW_fmt[j] == 0: continue
+                    print(f"\t\t\t{code} = {IW_fmt[j]};", file=f)
+                continue
+
+            if keys_data[i] in dict_fmt:
+                for j, code in enumerate(control_codes):
+                    if dict_fmt[keys_data[i]][j] == 0: continue
+                    print(f"\t\t\t{code} = {dict_fmt[keys_data[i]][j]};", file=f)
+                continue
+
+        print("\tend", file=f)
+        print("end", file=f)
+
+    print(f"[INFO] We wrote the controls to {out_file} succesfully!")
+
+
+if __name__ == "__main__":
+    main_control()
+
---
+++ DOCUMENTACION_PROYECTO/PRUEBAS_FINALES/ensamblador.py
@@ -0,0 +1,262 @@
+#!/usr/bin/python3
+from enum import Enum
+from pathlib import Path
+from ISA import instruction_map
+import argparse
+
+suffix = "\t | "
+class lType(Enum):
+    VACIO = 0
+    COMENTARIO = 1
+    TAG = 2
+    INSTRUCTION = 3
+
+def print_list(data:list, name="data"):
+    endstr = ", \n"
+    startstr = f"[DEBUG] {name} = ["
+    for i,el in enumerate(data):
+        if i == len(data)-1:
+            endstr = "]\n"
+        print(startstr, el, end=endstr)
+        if i == 0:
+            startstr = "            " + " "*len(name)
+
+def dump_code(data:list, file:str|None = None, binary=True):
+    if file is None:
+        print_list(data)
+        return
+    if binary:
+        with open(file, "w") as f:
+            for line in data:
+                print(line, file=f)
+    else:
+        with open(file, "w") as f:
+            print([int(a,2) for a in data], file=f)
+            # for line in data:
+            #     print(int(line, 2), file=f)
+
+def parse_line(line:str) -> (lType, tuple[str]):
+    """
+    Se devuelve (tipo:lType, argumentos:str)
+    No hace analisis semantico
+    """
+    line = line.strip()
+    if line == "": return (lType.VACIO, (""))
+    # Ignorar comentarios
+    com = line.find(";")
+    if com != -1:
+        comentario = line[com:]
+        line = line[:com]
+    if line == "": return (lType.COMENTARIO, (comentario))
+    # Checar si es una etiqueta
+    tag = line.find(":")
+    if tag != -1: return (lType.TAG, (line[:tag]))
+    # Entonces es un posible comando
+    line = line.lstrip()
+    ws = line.find(" ")
+    cmd = line[:ws]
+    line = line[ws:]
+    args = [arg.strip() for arg in line.split(",")]
+    args.insert(0, cmd)
+    return (lType.INSTRUCTION, args)
+
+def to2comp(num:int, size:int):
+    num = int(num)
+    signed = num < 0
+    if signed:
+        if abs(num) > 2**(size-1):
+            raise ValueError(f"Num '{num}' is too small to be represented by {size} bits")
+        b = bin(num + 1)
+        bnum = b[3:]
+    else:
+        if abs(num) > 2**(size-1) - 1:
+            raise ValueError(f"Num '{num}' is too big to be represented by {size} bits")
+        b = bin(num)
+        bnum = b[2:]
+
+    diff = size - len(bnum)
+    if diff > 0:
+        bnum = "0"*diff + bnum
+
+    if signed:
+        bnum = bnum.replace("0", "2")
+        bnum = bnum.replace("1", "0")
+        bnum = bnum.replace("2", "1")
+
+    return bnum
+
+def tobits(num:int, size:int) -> str:
+    b = bin(num)[2:]
+    diff = size - len(b)
+    if diff >= 0:
+        res = "0"*diff + b
+    if diff < 0:
+        raise ValueError(f"'{num}' can't be represented in a unsigned {size} bit number.")
+    return res
+
+def reg_to_bits(reg:str):
+    # Assumes reg is 2 char long, of the format xn
+    if reg[0].lower() != "x":
+        raise ValueError(f"Registros tienen nombre XN, no {reg[0]}.")
+    try:
+        num = int(reg[1:])
+    except Exception as e:
+        raise ValueError(f"{num} no es un numero")
+    if num < 0 or num > 31:
+        raise ValueError(f"No tenemos registro X{num}, el rango de registros es de X0 a X31.")
+    return tobits(num, 5)
+
+def parse_args(tipo:str, args:list[str], noLine:int, tags:dict[str, int]) -> list[str]:
+    def check_argument_len(nargs:int, desired:int, itype:str):
+        if nargs > desired:
+            raise ValueError(f"Too many arguments ({args = }) for {itype} type instruction, expected {desired}.")
+        if nargs < desired:
+            raise ValueError(f"Too few arguments ({args = }) for {itype} type instruction, expected {desired}.")
+
+    result = []
+    # Se asume que args tiene tamaño >= 1
+    match tipo:
+        case "B":
+            check_argument_len(len(args), 1, "B")
+            tag = args[0].strip()
+            if not tag in tags:
+                raise ValueError(f"There is no tag '{tag}' in source.")
+            offset = tags[tag] - (noLine + 1)
+            result.append( to2comp(offset, 26) )
+        case "CB":
+            check_argument_len(len(args), 2, "CB")
+            # offset
+            tag = args[1].strip()
+            if not tag in tags:
+                raise ValueError(f"There is no tag '{tag}' in source.")
+            offset = tags[tag] - noLine
+            result.append( to2comp(offset, 19) )
+            # register
+            result.append( reg_to_bits(args[0].strip()) )
+        case "D":
+            check_argument_len(len(args), 3, "D")
+            # address
+            addr = args[2].replace("]","").replace("#","").strip()
+            result.append( tobits(int(addr), 9) )
+            # op
+            result.append( tobits(0, 2) )
+            # Rn
+            reg = args[1].replace("[","").strip()
+            result.append( reg_to_bits(reg) )
+            # Rt
+            result.append( reg_to_bits(args[0].strip()) )
+        case "R":
+            check_argument_len(len(args), 3, "R")
+            # Rm
+            result.append( reg_to_bits(args[1].strip()) )
+            # shamt
+            result.append( tobits(0, 6) )
+            # Rn
+            result.append( reg_to_bits(args[2].strip()) )
+            # Rd
+            result.append( reg_to_bits(args[0].strip()) )
+        case "I":
+            check_argument_len(len(args), 3, "I")
+            # inmmediate
+            result.append( to2comp( int(args[2].replace("#","").strip()), 12) )
+            # Rn
+            result.append( reg_to_bits( args[1].strip()) )
+            # Rd
+            result.append( reg_to_bits( args[0].strip()) )
+        case "IW":
+            check_argument_len(len(args), 3, "IW")
+            # MOVZ x2, #immediate, 3
+            #                      ^ region
+            # region
+            result.append( tobits( int(args[2].strip()), 2) )
+            # immediate
+            result.append( to2comp( int(args[1].replace("#","").strip()), 16) )
+            # Rd
+            result.append( reg_to_bits( args[0].strip()) )
+        case _:
+            raise NotImplementedError(f"Tipo de instruccion \"{tipo}\" no implementado.")
+    return result
+
+
+def main(file, dump = None, binary=False):
+    tags = dict()
+    instructions = []
+    for_later = []
+    lines = []
+    with open(file, "r") as f:
+        for i,line in enumerate(f):
+            res = parse_line(line)
+            if res[0] == lType.VACIO or res[0] == lType.COMENTARIO:
+                continue
+            if res[0] == lType.TAG:
+                tags[res[1]] = i
+            if res[0] == lType.INSTRUCTION:
+                for_later.append((i,res[1]))
+                lines.append(line)
+
+    # Data hazards
+    NOP = ["ADD", "X31", "X31", "X31"]
+    for i,inst in enumerate(for_later):
+        noLine = inst[0]
+        cmd = inst[1][0]
+        args = inst[1][1:]
+        if not cmd in instruction_map:
+            raise ValueError(f"[ERROR] {file}:{noLine}. {cmd} no es una instruccion soportada.")
+        if len(args) == 0:
+            raise ValueError(f"[ERROR] {file}:{noLine}. {cmd} no tiene suficientes argumentos.")
+
+        if cmd == "B":
+            for_later.insert(i+1, (noLine+1, NOP))
+            for_later.insert(i+1, (noLine+2, NOP))
+
+    # Add a NOP at the end
+    for_later.append((len(for_later), NOP))
+
+    # Ahora que ya sabemos todas las tags podemos trabajar
+    for i,inst in enumerate(for_later):
+        noLine = inst[0]
+        cmd = inst[1][0]
+        args = inst[1][1:]
+        inst_type, opcode = instruction_map[cmd]
+
+        try:
+            args_list = parse_args(inst_type, args, noLine, tags)
+        except Exception as e:
+            print(f"[ERROR] {file}:{noLine}. Couldn't parse instruction:")
+            print(f"{suffix}{e}")
+            print(f"{suffix}{noLine} > '{lines[i][:-1]}'")
+            break
+
+        args_list.insert(0, opcode )
+        if binary:
+            code = " ".join(args_list)
+        else:
+            code = "".join(args_list)
+        instructions.append( code )
+
+
+    if dump is None:
+        dump = f"{file.stem}.out"
+    dump_code(instructions, dump, binary)
+    print(f"[INFO] '{file}' was assemble successfully!")
+    # print_list([len(ins) for ins in instructions])
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Assemble assembly code.')
+    parser.add_argument('filename', type=str, nargs="+",
+                        help='ARM(legv8) assembly code to assemble.')
+    parser.add_argument("-o",'--out', type=str, nargs="*", help='Output file to dump native code. (default="filename".out)')
+    parser.add_argument("-b",'--binary', action="store_true", default=False)
+    args = parser.parse_args()
+
+    if args.out is not None:
+        if len(args.out) != len(args.filename):
+            print(f"[ERROR] The outfile parameters(size={len(args.out)}) doesn't have the same size as the infile parameters(size={len(args.filename)}).")
+            exit(1)
+        outs = args.out
+    else:
+        outs = [None]*len(args.filename)
+    # print(args.out, args.filename, sep="\n")
+    # exit(69)
+    for i,file in enumerate(args.filename):
+        main(Path(file), outs[i], args.binary)
